0b04d77151213229261f85196e5dca8a4b965226
dchf <dchf.jp@gmail.com>
2024-02-05 22:33:40 +0900

    query google places api

--- meander/query.go
+++ meander/query.go
@@ -2,6 +2,10 @@ package meander
 
 import (
 	_ "embed"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/url"
 )
 
 //go:embed google_places_key
@@ -42,4 +46,39 @@ func (p *Place) Public() any {
 		"lat":      p.Lat,
 		"lng":      p.Lng,
 	}
+}
+
+type Query struct {
+	Lat          float64
+	Lng          float64
+	Journey      []string
+	Radius       int
+	CostRangeStr string
+}
+
+func (q *Query) find(types string) (*googleResponse, error) {
+	u := "https://maps.googleapis.com/maps/api/place/nearbysearch/json"
+
+	vals := make(url.Values)
+	vals.Set("location", fmt.Sprintf("%g,%g", q.Lat, q.Lng))
+	vals.Set("radius", fmt.Sprintf("%d", q.Radius))
+	vals.Set("type", types)
+	vals.Set("key", APIKey)
+	if len(q.CostRangeStr) > 0 {
+		r := ParseCostRange(q.CostRangeStr)
+		vals.Set("minprice", fmt.Sprintf("%d", int(r.From)-1))
+		vals.Set("maxprice", fmt.Sprintf("%d", int(r.To)-1))
+	}
+
+	res, err := http.Get(u + "?" + vals.Encode())
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	var response googleResponse
+	if err := json.NewDecoder(res.Body).Decode(&response); err != nil {
+		return nil, err
+	}
+	return &response, nil
 }
