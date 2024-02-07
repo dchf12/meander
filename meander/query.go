22a3144565049fc7f645ff24aa245c790c8e2ae8
dchf <dchf.jp@gmail.com>
2024-02-08 00:26:44 +0900

    create recommend

--- meander/query.go
+++ meander/query.go
@@ -4,8 +4,10 @@ import (
 	_ "embed"
 	"encoding/json"
 	"fmt"
+	"math/rand"
 	"net/http"
 	"net/url"
+	"sync"
 )
 
 //go:embed google_places_key
@@ -81,4 +83,31 @@ func (q *Query) find(types string) (*googleResponse, error) {
 		return nil, err
 	}
 	return &response, nil
+}
+
+func (q *Query) Run() any {
+	var wg sync.WaitGroup
+	places := make([]any, len(q.Journey))
+	for i, r := range q.Journey {
+		wg.Add(1)
+		go func(types string, i int) {
+			defer wg.Done()
+			resp, err := q.find(types)
+			if err != nil {
+				return
+			}
+			if len(resp.Results) == 0 {
+				return
+			}
+			for _, result := range resp.Results {
+				for _, photo := range result.Photos {
+					photo.URL = fmt.Sprintf("https://maps.googleapis.com/maps/api/place/photo"+
+						"?maxwidth=1000&photoreference=%s&key=%s", photo.PhotoRef, APIKey)
+				}
+			}
+			places[i] = resp.Results[rand.Intn(len(resp.Results))]
+		}(r, i)
+	}
+	wg.Wait()
+	return places
 }
